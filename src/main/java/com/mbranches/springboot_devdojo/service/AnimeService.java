fae4d8ffd378eb5c2e463f4c04b9686c329f0263
Branches <marcus.branches@icloud.com>
2025-02-26 19:46:34 -0300

    feat: adicionando o metodo post

--- src/main/java/com/mbranches/springboot_devdojo/service/AnimeService.java
+++ src/main/java/com/mbranches/springboot_devdojo/service/AnimeService.java
@@ -5,13 +5,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Collectors;
 
 @Service
 public class AnimeService {
-    private List<Anime> animes = List.of(new Anime(1L, "Boku No Hero"), new Anime(2L, "Berserk"));
+    private static List<Anime> animes = new ArrayList<>(List.of(new Anime(1L, "Boku No Hero"), new Anime(2L, "Berserk")));
 
     // private final AnimeRepository animeRepository;
     public List<Anime> listAll() {
@@ -23,5 +25,11 @@ public class AnimeService {
                 .filter(anime -> anime.getId().equals(id))
                 .findFirst()
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not Found"));
+    }
+
+    public Anime save(Anime anime) {
+        anime.setId(ThreadLocalRandom.current().nextLong(3, 100000));
+        animes.add(anime);
+        return anime;
     }
 }
