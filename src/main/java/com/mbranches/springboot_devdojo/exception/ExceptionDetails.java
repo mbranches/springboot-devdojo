ac307967215db1c9183198bfc9724cfe4a2503d6
Branches <marcus.branches@icloud.com>
2025-03-02 17:37:28 -0300

    feat: adicionando handler para errors de campos vazios

--- src/main/java/com/mbranches/springboot_devdojo/exception/ExceptionDetails.java
+++ src/main/java/com/mbranches/springboot_devdojo/exception/ExceptionDetails.java
@@ -4,12 +4,15 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.Getter;
 import lombok.experimental.SuperBuilder;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.time.LocalDateTime;
 
-@Getter
+@Data
 @SuperBuilder
-public class BadRequestDetails extends ExceptionDetails {
+public class ExceptionDetails {
+    protected String title;
+    protected int status;
+    protected String details;
+    protected String developerMessage;
+    protected LocalDateTime timestamp;
 }
--- src/main/java/com/mbranches/springboot_devdojo/handler/RestExceptionHandler.java
+++ src/main/java/com/mbranches/springboot_devdojo/handler/RestExceptionHandler.java
@@ -2,12 +2,17 @@ package com.mbranches.springboot_devdojo.handler;
 
 import com.mbranches.springboot_devdojo.exception.BadRequestDetails;
 import com.mbranches.springboot_devdojo.exception.BadRequestException;
+import com.mbranches.springboot_devdojo.exception.ValidationExceptionDetails;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class RestExceptionHandler {
@@ -20,6 +25,30 @@ public class RestExceptionHandler {
                         .title("Bad Request Exception, Check the Documentation")
                         .details(exception.getMessage())
                         .developerMessage(exception.getClass().getName())
+                        .build()
+                , HttpStatus.BAD_REQUEST
+        );
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationExceptionDetails> handlerMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+        List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
+        String fields = fieldErrors.stream()
+                .map(FieldError::getField)
+                .collect(Collectors.joining(", "));
+        String fieldsMessage = fieldErrors.stream()
+                .map(FieldError::getDefaultMessage)
+                .collect(Collectors.joining(", "));
+
+        return new ResponseEntity<>(
+                ValidationExceptionDetails.builder()
+                        .timestamp(LocalDateTime.now())
+                        .status(HttpStatus.BAD_REQUEST.value())
+                        .title("Bad Request Exception, Invalid Fields")
+                        .details("Check the field(s) error")
+                        .developerMessage(exception.getClass().getName())
+                        .fields(fields)
+                        .fieldsMessage(fieldsMessage)
                         .build()
                 , HttpStatus.BAD_REQUEST
         );
