cf51f34ff3c5afedb1e96adbe7a8f5c9cf950921
Branches <marcus.branches@icloud.com>
2025-03-04 11:49:52 -0300

    feat: adicionando teste jpa pt1 - metodo save

---
+++ src/test/java/com/mbranches/springboot_devdojo/repository/AnimeRepositoryTest.java
@@ -0,0 +1,35 @@
+package com.mbranches.springboot_devdojo.repository;
+
+import com.mbranches.springboot_devdojo.model.Anime;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ActiveProfiles;
+
+@DataJpaTest
+@DisplayName("Test for anime repository")
+class AnimeRepositoryTest {
+    @Autowired
+    private AnimeRepository animeRepository;
+
+    @Test
+    @DisplayName("Save persists anime when Successful")
+    void save_PersistAnime_WhenSuccessful() {
+        Anime animeToBeSaved = createAnime();
+
+        Anime animeSaved = this.animeRepository.save(animeToBeSaved);
+
+        Assertions.assertThat(animeSaved).isNotNull();
+        Assertions.assertThat(animeSaved.getId()).isNotNull();
+        Assertions.assertThat(animeSaved.getName()).isEqualTo(animeToBeSaved.getName());
+    }
+
+    private Anime createAnime() {
+        return Anime.builder()
+                .name("Naruto")
+                .build();
+    }
+}
