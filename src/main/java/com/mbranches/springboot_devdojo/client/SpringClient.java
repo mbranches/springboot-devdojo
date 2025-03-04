ba75e1c4ed1d2733ee1be7e00bf84f2ef0509d76
Branches <marcus.branches@icloud.com>
2025-03-04 10:47:13 -0300

    feat: testando metodos post, put e delete com RestTemplate

--- src/main/java/com/mbranches/springboot_devdojo/client/SpringClient.java
+++ src/main/java/com/mbranches/springboot_devdojo/client/SpringClient.java
@@ -4,6 +4,7 @@ import com.mbranches.springboot_devdojo.model.Anime;
 import lombok.extern.log4j.Log4j2;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
@@ -24,5 +25,27 @@ public class SpringClient {
         ResponseEntity<List<Anime>> exchange = new RestTemplate().exchange("http://localhost:8090/animes/all", HttpMethod.GET, null, new ParameterizedTypeReference<>() {
         });
         log.info(exchange.getBody());
+
+//        Anime kingdom = Anime.builder().name("Kingdom").build();
+//        Anime kingdomSaved = new RestTemplate().postForObject("http://localhost:8090/animes", kingdom, Anime.class);
+//        log.info("Saved anime {}", kingdomSaved);
+
+//        Anime samuraiChamploo = Anime.builder().name("Samurai Champloo").build();
+//        ResponseEntity<Anime> samuraiChamplooSaved = new RestTemplate().exchange("http://localhost:8090/animes", HttpMethod.POST, new HttpEntity<>(samuraiChamploo), Anime.class);
+//        log.info("Saved anime {}", samuraiChamplooSaved);
+//
+//        Anime samuraiChamplooAltered = samuraiChamplooSaved.getBody();
+//        samuraiChamplooAltered.setName("Samurai Champloo 2");
+
+
+//        ResponseEntity<Void> alterationResponse = new RestTemplate().exchange("http://localhost:8090/animes", HttpMethod.PUT, new HttpEntity<>(samuraiChamplooAltered), Void.class);
+
+//        log.info(alterationResponse);
+
+        new RestTemplate().exchange("http://localhost:8090/animes/{id}", HttpMethod.DELETE, null, Void.class, 21);
+
+        new RestTemplate().exchange("http://localhost:8090/animes/{id}", HttpMethod.DELETE, null, Void.class, 22);
+
     }
+
 }
